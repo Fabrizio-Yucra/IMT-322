02e9cfad839ea72f867647f155688c1700fff2bc
Fabrizio-Yucra <fabrizio.alessandro0@gmail.com>
2024-03-02 11:53:35 -0400

    Pracctica2

---
+++ Ejercicio3/Ejercicio3.ino
@@ -0,0 +1,20 @@
+#define BOTON 2
+int cont = 0;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BOTON, INPUT_PULLUP);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(BOTON), Incremento, FALLING);// esta funcion nos sirve para interrumpir el codigo cada vez que se detecte un cambio en el boton llamando a la funcion incremento cuando sea un flanco de bajada
+}
+void loop() {
+  Serial.println(cont);  
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);                    
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(1000);           
+}
+void Incremento()
+{
+  cont = cont +1;
+}
