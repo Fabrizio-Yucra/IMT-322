37d39e242740790574e8a8026440fa06c727b029
Fabrizio-Yucra <fabrizio.alessandro0@gmail.com>
2024-04-27 13:11:29 -0400

    Examen Maquina expendedora de bebidas

--- Ejercicio2/Ejercicio2.ino
+++ Ejercicio2/Ejercicio2.ino
@@ -2,39 +2,17 @@
 #define LED2 9
 #define LED3 10
 #define SPEED 200
-#define BOTON 11
+#define BOTON 2
 bool E = true;
 void setup() {
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
+  Serial.begin(9600);
   pinMode(BOTON, INPUT_PULLUP);
 }
 void loop() {
   if(digitalRead(BOTON) == 0){
-    E = !E;
+   
+    Serial.println("hola");
     delay(80);
   }
-  if(E == true){
-    digitalWrite(LED1, HIGH); 
-    delay(SPEED);                   
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, HIGH); 
-    delay(SPEED);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED3, HIGH); 
-    delay(SPEED);
-    digitalWrite(LED3, LOW);
-  }
-  if(E == false){
-    digitalWrite(LED3, HIGH); 
-    delay(SPEED);                   
-    digitalWrite(LED3, LOW);
-    digitalWrite(LED2, HIGH); 
-    delay(SPEED);
-    digitalWrite(LED2, LOW);
-    digitalWrite(LED1, HIGH); 
-    delay(SPEED);
-    digitalWrite(LED1, LOW);
-  }             
+              
 }
